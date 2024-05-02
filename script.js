7a2c045759765096c728f4b117d0239eefd93f87
ktorres63 <ktorresaro@unsa.edu.pe>
2024-05-01 22:57:26 -0500

    Fix zero in display

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 const display = document.querySelector(".display")
-const btns = document.querySelectorAll(".btn")
+const btns = document.querySelectorAll(".num")
+const numbers = document.querySelector(".numbers")
+const equal = document.querySelector(".equal")
+const reset = document.querySelector(".AC")
+
+let displayNums = []
+
+
 
 function write(){
 }
 
+
 btns.forEach(btn =>{
   btn.addEventListener("click", ()=> {
-    console.log(btn.textContent)
+    if(displayNums.length === 0){
+      numbers.textContent = ""
+    }
+    displayNums.push(btn.textContent)
+    numbers.textContent += btn.textContent;
+    // console.log(btn.textContent)
   });
 });
 
+equal.addEventListener("click",() => console.log(displayNums))
+reset.addEventListener("click",() => location.reload())
 
-
-// const btn = document.querySelector(".seven")
-// btn.addEventListener("click", ()=> {
-//   console.log(btn.textContent)
-// })
