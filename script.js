c5b46d11fc2333766a427032515ff7a198c2b224
ktorres63 <ktorresaro@unsa.edu.pe>
2024-05-01 22:07:02 -0500

    Print text content for every btn

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("hello world")
+const display = document.querySelector(".display")
+const btns = document.querySelectorAll(".btn")
+
+function write(){
+}
+
+btns.forEach(btn =>{
+  btn.addEventListener("click", ()=> {
+    console.log(btn.textContent)
+  });
+});
+
+
+
+const btn = document.querySelector(".seven")
+// btn.addEventListener("click", ()=> {
+//   console.log(btn.textContent)
+// })
